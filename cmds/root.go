56b94de54eff658c84cd8db389e1aff6848e1198
kooksee <kooksee@163.com>
2020-03-16 20:27:00 +0800

    fix: add webview template

--- cmds/root.go
+++ cmds/root.go
@@ -3,6 +3,7 @@ package cmds
 import (
 	"github.com/pubgo/antproxy/cmds/antproxy"
 	"github.com/pubgo/antproxy/cmds/mkcert"
+	"github.com/pubgo/antproxy/cmds/wv"
 	"github.com/pubgo/antproxy/version"
 	"github.com/pubgo/xcmd/xcmd"
 )
@@ -17,5 +18,6 @@ var Execute = xcmd.Init(func(cmd *xcmd.Command) {
 	cmd.AddCommand(
 		mkcert.Init(),
 		antproxy.Init(),
+		wv.Init(),
 	)
 })
